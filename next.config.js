31c9fa45778586a622ee7e35e7822b89be0f0209
fullstack-guy <andriihryhoriev1@gmail.com>
2023-06-21 17:22:42 -0400

    changed config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   exportPathMap: function () {
     return {
-      '/': { page: '/index' },
+      '/': { page: '/index' }, //fixing
     };
   },
 };
