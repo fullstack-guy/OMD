79e61d7f4d1b773cbeca5da36ee1c2887e822932
fullstack-guy <andriihryhoriev1@gmail.com>
2023-06-27 14:53:09 -0400

    try to merge to main branch

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  exportPathMap: function () {
-    return {
-      '/': { page: '/index' }, 
-    };
-  },
+ 
   trailingSlash: true, 
 };
 
