2d7ca1394589b2956ed2e8ba1c4600012d446bed
fullstack-guy <andriihryhoriev1@gmail.com>
2023-06-28 16:09:59 -0400

    deploy testing

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
- 
-  trailingSlash: true, 
+const nextConfig   = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
 };
-
 
 module.exports = nextConfig;
