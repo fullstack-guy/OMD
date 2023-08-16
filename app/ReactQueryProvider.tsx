83c33281d2713e78efc47979f1b9baa553186eec
fullstack-guy <andriihryhoriev1@gmail.com>
2023-08-16 03:15:42 -0700

    fix the type

--- app/ReactQueryProvider.tsx
+++ app/ReactQueryProvider.tsx
@@ -3,7 +3,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
 
-export const ReactQueryProvider: React.FC = ({ children }) => {
+export const ReactQueryProvider: React.FC<{
+  children: React.ReactNode
+}> = ({ children }) => {
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   )
