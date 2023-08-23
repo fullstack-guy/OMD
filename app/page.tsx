3008d38cfabbf5a686b6a71bf11bde9fbe730c81
fullstack-guy <andriihryhoriev1@gmail.com>
2023-08-23 13:52:07 -0400

    'merge

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image'
-
+import Link from 'next/link'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Login page</h1>
+      <Link href="/my-doctor">
+        go to my doctor page
+      </Link>
     </main>
   )
 }
