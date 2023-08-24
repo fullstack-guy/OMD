87f99a25fa64db5b53fa5e5795c2235bb179c2d2
fullstack-guy <andriihryhoriev1@gmail.com>
2023-08-24 13:02:06 -0400

    'solving'

--- app/my-doctor/saved-doctors/page.jsx
+++ app/my-doctor/saved-doctors/page.jsx
@@ -1,51 +1,41 @@
+'use client'
 
+import React, { useState, useEffect } from 'react';
 import SavedPageCard from "@/components/SavedPageCard";
+ 
 
-
-export default function Saved({ data }) {
- if(!data){
-    return( 
-        <div className="">
-            loading....
-        </div>
-    )
- }
-   
-  return (
-    <div className="container">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 m-6">
-        {data?.results?.map((post, index) => (
-          <SavedPageCard
-            key={index}
-            Name={post.firstName}
-            Job={post.primarySpecialty}
-            npi={String(post.npi)}
-            id={post.internal_id}
-          />
-        ))}
-      </div>
-    </div>
-  );
-}
-
-export async function getServerSideProps() {
-    try {
-      const API_DOCTOR = process.env.API_KEY;
-     
-      const res = await fetch(API_DOCTOR);
-      const data = await res.json();
+export default function Saved() {
+    const [data, setData] = useState([]);
   
-      return {
-        props: {
-          data,
-        },
-      };
-    } catch (error) {
-      console.error("Error fetching API data:", error);
-      return {
-        props: {
-          data: null,
-        },
-      };
-    }
-  }
+    useEffect(() => {
+        async function fetchData() {
+            
+            try {
+                var API_NEW = process.env.API_KEY;
+                const res = await fetch(API_NEW);
+                const apiData = await res.json();
+                setData(apiData.results);
+            } catch (error) {
+                console.error("Error fetching API data:", error);
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    return (
+        <div className="container">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 m-6">
+                {data.map((post, index) => (
+                    <SavedPageCard
+                        key={index}
+                        Name={post.firstName}
+                        Job={post.primarySpecialty}
+                        npi={String(post.npi)}
+                        id={post.internal_id}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
