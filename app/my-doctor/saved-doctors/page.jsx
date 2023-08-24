191a31351395218ae3c38a7182c3e9b235da51e3
fullstack-guy <andriihryhoriev1@gmail.com>
2023-08-24 13:08:02 -0400

    'ss'

--- app/my-doctor/saved-doctors/page.jsx
+++ app/my-doctor/saved-doctors/page.jsx
@@ -23,6 +23,13 @@ export default function Saved() {
         fetchData();
     }, []);
 
+    if(!data){
+        return(
+            <div>
+                loading....
+            </div>
+        )
+    }
     return (
         <div className="container">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 m-6">
